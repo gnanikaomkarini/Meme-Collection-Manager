72e1ce82220b4c9440a7dfdebaaec3eeb38cfb9a
Gnanika Omkarini Makkena <gnanikaomkarini@gmail.com>
2026-01-27 20:56:45 +0530

    backend: defined the User model

---
+++ backend/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    googleId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    displayName:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    profileImage:{
+        type: String
+    },
+}, { 
+    timestamps: true 
+});
+
+module.exports = mongoose.model('User', userSchema);
